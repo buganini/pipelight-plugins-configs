6e21f06fcc3a744845d28f8e7c6a83a793756441
Buganini Q <buganini@gmail.com>
2014-09-29 01:48:43 +0800

    fix bug in install script

--- util.sh
+++ util.sh
@@ -11,16 +11,16 @@ case $1 in
 		echo "3) restart browser"
 	;;
 	"install")
-		plugin=$2;
+		plugin=$2
+		cp pipelight-${plugin} /usr/share/pipelight/configs/
 		cd /usr/lib/pipelight/ && cp libpipelight.so libpipelight-${plugin}.so
 		cd /usr/lib/mozilla/plugins/ && ln -s /usr/lib/pipelight/libpipelight-${plugin}.so libpipelight-${plugin}.so
-		cp pipelight-${plugin} /usr/share/pipelight/configs
 		echo "0) read detail in README.md"
 		echo "1) install your program"
 		echo "2) restart browser"
 	;;
 	"remove")
-		plugin=$2;
+		plugin=$2
 		rm -f /usr/lib/pipelight/libpipelight-${plugin}.so
 		rm -f /usr/lib/mozilla/plugins/libpipelight-${plugin}.so
 		rm -f /usr/share/pipelight/configs/pipelight-${plugin}
